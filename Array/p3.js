a899323afa20606aa193a1a61b020ba962ff8871
TangGoong <791422364@qq.com>
2021-03-21 23:41:31 +0800

    main

--- Array/p3.js
+++ Array/p3.js
@@ -5,24 +5,63 @@
     具体某一周平均数
     所有周平均数
 */
+
 function weekTemps() {
-    this.dataStore = iniWeeks;
-    this.add = add;
-}
-function iniWeeks(row,col,initial) {
-    let arr = [];
-    for (let i = 0;i < row; ++i) {
-        let columns = [];
-        for (let j = 0;j < col; ++j) {
-            columns[j] = initial;
-        }
-        arr[i] = columns
-    }
-    return arr;
-}
-function add () {
-    
+    this.dataStore = [
+        [48,51,46,54,55,56,53],
+        [56,50,66,64,53,56,53],
+        [68,51,46,54,65,53,54],
+        [58,55,45,64,57,54,53]];
+    this.monthAvg = monthAvg;
+    this.someWeekAvg = someWeekAvg;
+    this.allWeekAvg = allWeekAvg;
 }
 
-weekTemps.matrix(4,7,0)
-console.log();
+function monthAvg () {
+    //console.log(this.dataStore.length);
+    let total = 0;
+    let arr = this.dataStore;
+    let length = 0;
+    for (let i = 0;i < arr.length;i++){
+        for (let j = 0;j < arr[i].length;j++) {
+            total += arr[i][j];
+            length ++;
+        }
+    }
+    let average = total / length;
+    return average.toFixed(0);
+}
+function someWeekAvg (week) {
+    //console.log(this.dataStore.length);
+    let total = 0;
+    let arr = this.dataStore;
+    if (arr[week]) {
+       for (let i = 0;i < arr[week].length;i++) {
+            total += arr[week][i];
+        }
+        let average = total / arr[week].length;
+        return average.toFixed(0); 
+    } else {
+        return "周数有误"
+    } 
+}
+function allWeekAvg () {
+    //console.log(this.dataStore.length);
+    let arr = this.dataStore;
+    let weekarr = [];
+    for (let i = 0;i < arr.length;i++){
+        let total = 0;
+        for (let j = 0;j < arr[i].length;j++) {
+            total += arr[i][j];
+        }
+        let average = total / arr[i].length;
+        weekarr.push(average.toFixed(0));
+    }
+    
+    return weekarr;
+}
+let weekTemp = new weekTemps();
+
+console.log(weekTemp.monthAvg());
+console.log(weekTemp.someWeekAvg(0));
+console.log(weekTemp.allWeekAvg());
